8142fc86fce7eb7142e223afdf1a38aba4392aae
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-10-22 18:29:24 -0500

    update(IAM): user auth attributes.

--- IoBuilt.API/IAM/Domain/Model/Aggregates/User.cs
+++ IoBuilt.API/IAM/Domain/Model/Aggregates/User.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace IoBuilt.API.IAM.Domain.Model.Aggregates;
 
 /// <summary>
@@ -7,24 +9,50 @@ namespace IoBuilt.API.IAM.Domain.Model.Aggregates;
 /// This class represents the User aggregate root.
 /// It contains the properties and methods to manage the user information.
 /// </remarks>
-public partial class User
+public partial class User(string email, string passwordHash, string role)
 {
-    public int Id { get; }
-    public string Email { get; private set; }
-    public string Password { get; private set; }
-    public string Role { get; private set; }
-
-    public User()
+    public User() : this(string.Empty, string.Empty, "User")
     {
-        Email = string.Empty;
-        Password = string.Empty;
-        Role = string.Empty;
     }
+
+    public int Id { get; }
+    public string Email { get; private set; } = email;
     
-    public User(string email, string password, string role)
+    [JsonIgnore]
+    public string PasswordHash { get; private set; } = passwordHash;
+    
+    public string Role { get; private set; } = role;
+
+    /// <summary>
+    /// Update the email
+    /// </summary>
+    /// <param name="email">The new email</param>
+    /// <returns>The updated user</returns>
+    public User UpdateEmail(string email)
     {
         Email = email;
-        Password = password;
+        return this;
+    }
+
+    /// <summary>
+    /// Update the password hash
+    /// </summary>
+    /// <param name="passwordHash">The new password hash</param>
+    /// <returns>The updated user</returns>
+    public User UpdatePasswordHash(string passwordHash)
+    {
+        PasswordHash = passwordHash;
+        return this;
+    }
+    
+    /// <summary>
+    /// Update the role
+    /// </summary>
+    /// <param name="role">The new role</param>
+    /// <returns>The updated user</returns>
+    public User UpdateRole(string role)
+    {
         Role = role;
+        return this;
     }
 }
--- IoBuilt.API/IAM/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
+++ IoBuilt.API/IAM/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -13,7 +13,7 @@ public static class ModelBuilderExtensions
         builder.Entity<User>().HasKey(u => u.Id);
         builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(100);
-        builder.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(255);
+        builder.Entity<User>().Property(u => u.PasswordHash).IsRequired().HasMaxLength(255);
         builder.Entity<User>().Property(u => u.Role).IsRequired().HasMaxLength(50);
     }
 }
