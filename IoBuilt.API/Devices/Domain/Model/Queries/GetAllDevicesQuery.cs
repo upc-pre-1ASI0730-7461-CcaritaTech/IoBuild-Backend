37cec03e21e4b831ad1e9525a62ca461a6423a2f
F4brizio24 <fabriziompc.240503@gmail.com>
2025-11-05 01:03:50 -0500

    feat(devices): add query records for retrieving devices

---
+++ IoBuilt.API/Devices/Domain/Model/Queries/GetAllDevicesQuery.cs
@@ -0,0 +1,3 @@
+namespace IoBuilt.API.Devices.Domain.Model.Queries;
+
+public record GetAllDevicesQuery();
---
+++ IoBuilt.API/Devices/Domain/Model/Queries/GetDeviceByIdQuery.cs
@@ -0,0 +1,3 @@
+namespace IoBuilt.API.Devices.Domain.Model.Queries;
+
+public record GetDeviceByIdQuery(int DeviceId);
