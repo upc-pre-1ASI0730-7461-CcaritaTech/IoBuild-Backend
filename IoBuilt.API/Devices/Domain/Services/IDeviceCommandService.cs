507ec23b54f776f9b5b7c51cb1059b9569212e44
Yeira <lleuxmerveivp@gmail.com>
2025-11-05 10:33:06 -0500

    feat(devices): add domain layer init config.

--- IoBuilt.API/Devices/Domain/Services/IDeviceCommandService.cs
+++ IoBuilt.API/Devices/Domain/Services/IDeviceCommandService.cs
@@ -1,9 +1,10 @@
-using IoBuilt.API.Devices.Domain.Model.Aggregates;
 using IoBuilt.API.Devices.Domain.Model.Commands;
 
 namespace IoBuilt.API.Devices.Domain.Services;
 
 public interface IDeviceCommandService
 {
-    Task<Device?> Handle(CreateDeviceCommand command);
+    Task<int> Handle(CreateDeviceCommand command);
+    Task Handle(UpdateDeviceCommand command);
+    Task Handle(DeleteDeviceCommand command);
 }
