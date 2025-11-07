1b29ebbabc71eb1bbc641281f16de5f0bfb185af
nOOmz <n00mr.axl@gmail.com>
2025-11-07 01:30:01 -0500

    feat(projects): add update and delete command handlers to IProjectCommandService

--- IoBuilt.API/Projects/Domain/Services/IProjectCommandService.cs
+++ IoBuilt.API/Projects/Domain/Services/IProjectCommandService.cs
@@ -6,4 +6,6 @@ namespace IoBuilt.API.Projects.Domain.Services;
 public interface IProjectCommandService
 {
     Task<Project?> Handle(CreateProjectCommand command);
+    Task<Project?> Handle(UpdateProjectCommand command);
+    Task<bool> Handle(DeleteProjectCommand command);
 }
