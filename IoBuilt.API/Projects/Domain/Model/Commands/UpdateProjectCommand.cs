43f999fec650ffaa048dc24ab354cdd8b358cca5
JeanArizabal <u201919096@upc.edu.pe>
2025-11-12 17:51:08 -0500

    feat(projects): refactor project status to use e project status enum in commands and repository.

--- IoBuilt.API/Projects/Domain/Model/Commands/UpdateProjectCommand.cs
+++ IoBuilt.API/Projects/Domain/Model/Commands/UpdateProjectCommand.cs
@@ -1,3 +1,5 @@
+using IoBuilt.API.Projects.Domain.Model.ValueObjects;
+
 namespace IoBuilt.API.Projects.Domain.Model.Commands;
 
-public record UpdateProjectCommand(int Id, string Name, string Description, string Location, int TotalUnits, int OccupiedUnits, string Status, int BuilderId, string ImageUrl);
+public record UpdateProjectCommand(int Id, string Name, string Description, string Location, int TotalUnits, int OccupiedUnits, EProjectStatus Status, int BuilderId, string ImageUrl);
