ee0ea473142dd10a19c1673f6111cc2d19066153
nOOmz <n00mr.axl@gmail.com>
2025-11-27 21:48:18 -0500

    feat(clients): add GetClientsByAccountStatementQuery for retrieving clients by account statement

---
+++ IoBuilt.API/Clients/Domain/Model/Queries/GetClientsByAccountStatementQuery.cs
@@ -0,0 +1,5 @@
+using IoBuilt.API.Clients.Domain.Model.ValueObjects;
+
+namespace IoBuilt.API.Clients.Domain.Model.Queries;
+
+public record GetClientsByAccountStatementQuery(EAccountStatement AccountStatement);
