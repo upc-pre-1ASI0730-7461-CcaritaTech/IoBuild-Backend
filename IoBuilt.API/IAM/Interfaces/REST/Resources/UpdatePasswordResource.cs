38cf9a29165a9a0c070c292e03a4161c7ef5e0ed
F4brizio24 <fabriziompc.240503@gmail.com>
2025-11-29 15:06:33 -0500

    feat(iam): add UpdatePasswordCommand and associated resource and assembler for password updates

---
+++ IoBuilt.API/IAM/Interfaces/REST/Resources/UpdatePasswordResource.cs
@@ -0,0 +1,8 @@
+namespace IoBuilt.API.IAM.Interfaces.REST.Resources;
+
+public class UpdatePasswordResource
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
