89aa561af73551a094c5d088a79086522d73b4eb
nOOmz <n00mr.axl@gmail.com>
2025-11-29 16:29:20 -0500

    feat(clients): remove unused methods from ClientRepository

--- IoBuilt.API/Clients/Infrastructure/Persistence/EFC/Repositories/ClientRepository.cs
+++ IoBuilt.API/Clients/Infrastructure/Persistence/EFC/Repositories/ClientRepository.cs
@@ -1,5 +1,4 @@
 using IoBuilt.API.Clients.Domain.Model.Aggregates;
-using IoBuilt.API.Clients.Domain.Model.ValueObjects;
 using IoBuilt.API.Clients.Domain.Repositories;
 using IoBuilt.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using IoBuilt.API.Shared.Infrastructure.Persistence.EFC.Repositories;
@@ -9,15 +8,6 @@ namespace IoBuilt.API.Clients.Infrastructure.Persistence.EFC.Repositories;
 
 public class ClientRepository(AppDbContext context) : BaseRepository<Client>(context), IClientRepository
 {
-    public async Task<IEnumerable<Client>> FindByProjectIdAsync(int projectId)
-    {
-        return await Context.Set<Client>().Where(c => c.ProjectId == projectId).ToListAsync();
-    }
-
-    public async Task<IEnumerable<Client>> FindByAccountStatementAsync(EAccountStatement accountStatement)
-    {
-        return await Context.Set<Client>().Where(c => c.AccountStatement == accountStatement).ToListAsync();
-    }
 
     public async Task<Client?> FindByEmailAsync(string email)
     {
