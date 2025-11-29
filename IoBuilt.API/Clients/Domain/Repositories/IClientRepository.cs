6a7edb5d4c1e2bd1c640e96d7288d48789171ba7
nOOmz <n00mr.axl@gmail.com>
2025-11-29 16:31:09 -0500

    feat(clients): update connection string and add .dockerignore for development environment

--- IoBuilt.API/Clients/Domain/Repositories/IClientRepository.cs
+++ IoBuilt.API/Clients/Domain/Repositories/IClientRepository.cs
@@ -1,12 +1,9 @@
 using IoBuilt.API.Clients.Domain.Model.Aggregates;
-using IoBuilt.API.Clients.Domain.Model.ValueObjects;
 using IoBuilt.API.Shared.Domain.Repositories;
 
 namespace IoBuilt.API.Clients.Domain.Repositories;
 
 public interface IClientRepository : IBaseRepository<Client>
 {
-    Task<IEnumerable<Client>> FindByProjectIdAsync(int projectId);
-    Task<IEnumerable<Client>> FindByAccountStatementAsync(EAccountStatement accountStatement);
     Task<Client?> FindByEmailAsync(string email);
 }
