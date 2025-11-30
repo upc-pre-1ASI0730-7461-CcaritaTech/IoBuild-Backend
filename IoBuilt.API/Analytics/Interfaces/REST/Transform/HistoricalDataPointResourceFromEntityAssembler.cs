6eace292603ad28c0a2223f83911f82e898f33c9
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-11-30 18:48:01 -0500

    docs(analytics): enhance XML documentation for HistoricalDataPoint and related classes

--- IoBuilt.API/Analytics/Interfaces/REST/Transform/HistoricalDataPointResourceFromEntityAssembler.cs
+++ IoBuilt.API/Analytics/Interfaces/REST/Transform/HistoricalDataPointResourceFromEntityAssembler.cs
@@ -3,8 +3,16 @@ using IoBuilt.API.Analytics.Interfaces.REST.Resources;
 
 namespace IoBuilt.API.Analytics.Interfaces.REST.Transform;
 
+/// <summary>
+/// Assembler for transforming HistoricalDataPoint domain entity to HistoricalDataPointResource.
+/// </summary>
 public static class HistoricalDataPointResourceFromEntityAssembler
 {
+    /// <summary>
+    /// Transforms a HistoricalDataPoint domain entity to HistoricalDataPointResource.
+    /// </summary>
+    /// <param name="entity">The HistoricalDataPoint entity to transform.</param>
+    /// <returns>The transformed HistoricalDataPointResource.</returns>
     public static HistoricalDataPointResource ToResourceFromEntity(HistoricalDataPoint entity)
     {
         return new HistoricalDataPointResource(
