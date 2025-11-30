972ee3c657e9d2930de622ec0996bb2cb435abff
F4brizio24 <fabriziompc.240503@gmail.com>
2025-11-30 10:51:27 -0500

    feat(iam): add SecondEmail to ProfileResource and create SecondEmailResource

--- IoBuilt.API/Profiles/Interfaces/REST/Transform/ProfileResourceFromEntityAssembler.cs
+++ IoBuilt.API/Profiles/Interfaces/REST/Transform/ProfileResourceFromEntityAssembler.cs
@@ -15,6 +15,7 @@ public static class ProfileResourceFromEntityAssembler
             entity.Username, 
             entity.Address, 
             entity.Age, 
-            entity.PhoneNumber);
+            entity.PhoneNumber,
+            entity.SecondEmail);
     }
 }
