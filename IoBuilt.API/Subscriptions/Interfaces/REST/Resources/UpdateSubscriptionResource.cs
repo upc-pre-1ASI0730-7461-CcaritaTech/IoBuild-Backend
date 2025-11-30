414de6b80875d41f2d35cf24790dbed2d136d40a
nOOmz <n00mr.axl@gmail.com>
2025-11-29 22:50:06 -0500

    feat(subscriptions): update UpdateSubscriptionResource to use PlanId instead of Plan

--- IoBuilt.API/Subscriptions/Interfaces/REST/Resources/UpdateSubscriptionResource.cs
+++ IoBuilt.API/Subscriptions/Interfaces/REST/Resources/UpdateSubscriptionResource.cs
@@ -1,9 +1,7 @@
 namespace IoBuilt.API.Subscriptions.Interfaces.REST.Resources;
 
 public record UpdateSubscriptionResource(
-    string? Plan,
+    int? PlanId,
     string? Status,
     DateTime? StartDate,
-    DateTime? EndDate,
-    decimal? Price,
-    IEnumerable<string>? Features);
+    DateTime? EndDate);
